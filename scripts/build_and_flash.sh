23021991e6d65ccfac431ccb050acf9bfde094ad
DankersW <wouter.dankers@skynet.be>
2021-07-29 16:25:37 +0200

    toolchain: optimize build and flash stage

--- scripts/build_and_flash.sh
+++ scripts/build_and_flash.sh
@@ -24,12 +24,13 @@ fi
 
 BOARD="nrf52840dongle_nrf52840"
 HEX_FILE="build/zephyr/zephyr.hex"
+APP_NAME="$(cut -d'/' -f2 <<<$APP)"
 
 echo "Building application $APP"
 ./scripts/toolchain.sh west build -p -b $BOARD $APP || exit 1
 
 echo "Flashing hex"
-PKG_NAME="$APP.zip" 
+PKG_NAME="$APP_NAME.zip" 
 nrfutil pkg generate --hw-version 52 --sd-req=0x00 --application $HEX_FILE --application-version 1 $PKG_NAME
 nrfutil dfu usb-serial -pkg $PKG_NAME -p /dev/ttyACM0
 rm $PKG_NAME
