d27c5b3f658662edcbc8958ea6201469f6b7fac2
DankersW <wouter.dankers@skynet.be>
2021-08-02 15:18:07 +0200

    bt: attention models moved

---
+++ app/soil_moisture_meter/src/bt/attention_model_handlers.c
@@ -0,0 +1,32 @@
+#include "attention_model_handlers.h"
+
+static struct k_delayed_work attention_blink_work;
+
+void attention_blink(struct k_work *work)
+{
+	static int idx;
+	const uint8_t pattern[] = {
+		BIT(0) | BIT(1),
+		BIT(1) | BIT(2),
+		BIT(2) | BIT(3),
+		BIT(3) | BIT(0),
+	};
+	dk_set_leds(pattern[idx++ % ARRAY_SIZE(pattern)]);
+	k_delayed_work_submit(&attention_blink_work, K_MSEC(30));
+}
+
+void attention_on(struct bt_mesh_model *mod)
+{
+	k_delayed_work_submit(&attention_blink_work, K_NO_WAIT);
+}
+
+void attention_off(struct bt_mesh_model *mod)
+{
+	k_delayed_work_cancel(&attention_blink_work);
+	dk_set_leds(DK_NO_LEDS_MSK);
+}
+
+struct k_delayed_work *get_attention_blink_work()
+{
+	return &attention_blink_work;
+}
