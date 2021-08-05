f64b5b414b837c27e92da193ed90560e55b71997
DankersW <wouter.dankers@skynet.be>
2021-08-05 11:10:41 +0200

    sample: working log conf

--- samples/usb_print/src/main.c
+++ samples/usb_print/src/main.c
@@ -3,13 +3,41 @@
 
 #include <usb/usb_device.h>
 
+#include <logging/log.h>
+
+#define LOG_MODULE_NAME testnig
+LOG_MODULE_REGISTER(LOG_MODULE_NAME, LOG_LEVEL_DBG);
+
 void main(void)
 {
 	if (usb_enable(NULL)) {
 		return;
 	}
 
-	printk("All ready to roll...");
+	struct log_msg_ids src_level = {
+		.level = LOG_LEVEL_INTERNAL_RAW_STRING,
+		.source_id = 0, /* not used as level indicates raw string. */
+		.domain_id = 0, /* not used as level indicates raw string. */
+	};
+	
+	//printk("All ready to roll...");
+	//printk("hello sys-t on board\n");
+
+	/* standard print */
+	LOG_ERR("Error message example.");
+	LOG_WRN("Warning message example.");
+	LOG_INF("Info message example.");
+	LOG_DBG("Debug message example.");
+
+	LOG_DBG("Debug message example, %d", 1);
+	LOG_DBG("Debug message example, %d, %d", 1, 2);
+	LOG_DBG("Debug message example, %d, %d, %d", 1, 2, 3);
+	LOG_DBG("Debug message example, %d, %d, %d, 0x%x", 1, 2, 3, 4);
+
+	/* log output string */
+	log_string_sync(src_level, "%s", "log string sync");
+
+	
 	while (1) {
 		k_sleep(K_SECONDS(0.5));
 		printk("Hello world\n");
