a95c6238fdf3cb3a12f50cc5398da6b1c28349d0
DankersW <wouter.dankers@skynet.be>
2021-08-05 11:23:42 +0200

    sample: log to usb

--- samples/usb_print/src/main.c
+++ samples/usb_print/src/main.c
@@ -1,11 +1,9 @@
 #include <zephyr.h>
 #include <sys/printk.h>
-
 #include <usb/usb_device.h>
-
 #include <logging/log.h>
 
-#define LOG_MODULE_NAME testnig
+#define LOG_MODULE_NAME usb_print_example
 LOG_MODULE_REGISTER(LOG_MODULE_NAME, LOG_LEVEL_DBG);
 
 void main(void)
@@ -14,16 +12,6 @@ void main(void)
 		return;
 	}
 
-	struct log_msg_ids src_level = {
-		.level = LOG_LEVEL_INTERNAL_RAW_STRING,
-		.source_id = 0, /* not used as level indicates raw string. */
-		.domain_id = 0, /* not used as level indicates raw string. */
-	};
-	
-	//printk("All ready to roll...");
-	//printk("hello sys-t on board\n");
-
-	/* standard print */
 	LOG_ERR("Error message example.");
 	LOG_WRN("Warning message example.");
 	LOG_INF("Info message example.");
@@ -33,14 +21,9 @@ void main(void)
 	LOG_DBG("Debug message example, %d, %d", 1, 2);
 	LOG_DBG("Debug message example, %d, %d, %d", 1, 2, 3);
 	LOG_DBG("Debug message example, %d, %d, %d, 0x%x", 1, 2, 3, 4);
-
-	/* log output string */
-	log_string_sync(src_level, "%s", "log string sync");
-
 	
 	while (1) {
-		k_sleep(K_SECONDS(0.5));
+		k_sleep(K_SECONDS(2));
 		printk("Hello world\n");
 	}
-	
 }
