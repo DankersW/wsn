0a8e85dfc04c6c6b66175f671537fff0abbdc741
DankersW <wouter.dankers@skynet.be>
2021-09-02 14:44:28 +0200

    ot: coap client

--- app/thread_gateway/src/cli/handlers/ot_cmd_handlers.h
+++ app/thread_gateway/src/cli/handlers/ot_cmd_handlers.h
@@ -4,6 +4,8 @@
 #include <zephyr.h>
 #include <shell/shell.h>
 
+#include "coap_client_utils.h"
+
 int cmd_ot_start(const struct shell *shell, size_t argc, char **argv);
 
 #endif // OT_CMD_HANDLERS_H
