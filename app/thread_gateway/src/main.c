92bf75c5abb9b7aa0f9fa9e74543e366484051f8
DankersW <wouter.dankers@skynet.be>
2021-09-02 15:26:58 +0200

    ot: coap test

--- app/thread_gateway/src/main.c
+++ app/thread_gateway/src/main.c
@@ -52,7 +52,7 @@ static void on_button_changed(uint32_t button_state, uint32_t has_changed)
 
 	if (buttons & DK_BTN1_MSK) {
 		coap_client_toggle_mesh_lights();
-		
+		dk_set_led_on(0);
 	}
 
 	if (buttons & DK_BTN2_MSK) {
