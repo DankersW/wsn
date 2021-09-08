4a5729be9e5172b8ec202729626013388d0ba3d5
DankersW <wouter.dankers@skynet.be>
2021-09-08 16:31:58 +0200

    coap: ot coap definitions

---
+++ app/thread_gateway/src/ot_coap/ot_definitions.h
@@ -0,0 +1,17 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#include <stdio.h>
+
+#define LIGHT_URI_PATH "light"
+#define COAP_PORT 5683
+
+#define OT_CONNECTION_LED DK_LED1
+
+enum light_command {
+	THREAD_COAP_LIGHT_CMD_OFF = '0',
+	THREAD_COAP_LIGHT_CMD_ON = '1',
+	THREAD_COAP_LIGHT_CMD_TOGGLE = '2'
+};
+
+#endif // DEFINITIONS_H
