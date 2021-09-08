e2ffa551bfbe39ed3337325dbddd1bdbe7b4b98f
DankersW <wouter.dankers@skynet.be>
2021-09-08 16:59:27 +0200

    coap: removed dumb printout

--- app/thread_gateway/src/ot_coap/ot_coap_utils.c
+++ app/thread_gateway/src/ot_coap/ot_coap_utils.c
@@ -4,25 +4,27 @@ LOG_MODULE_REGISTER(ot_coap_utils, LOG_LEVEL_DBG);
 
 struct server_context {
 	struct otInstance *ot;
-	bool provisioning_enabled;
+	//bool provisioning_enabled;
 	light_request_callback_t on_light_request;
-	provisioning_request_callback_t on_provisioning_request;
+	//provisioning_request_callback_t on_provisioning_request;
 };
 
 static struct server_context srv_context = {
 	.ot = NULL,
-	.provisioning_enabled = false,
+	//.provisioning_enabled = false,
 	.on_light_request = NULL,
-	.on_provisioning_request = NULL,
+	//.on_provisioning_request = NULL,
 };
 
 /**@brief Definition of CoAP resources for provisioning. */
+/*
 static otCoapResource provisioning_resource = {
 	.mUriPath = PROVISIONING_URI_PATH,
 	.mHandler = NULL,
 	.mContext = NULL,
 	.mNext = NULL,
 };
+*/
 
 /**@brief Definition of CoAP resources for light. */
 static otCoapResource light_resource = {
@@ -32,6 +34,7 @@ static otCoapResource light_resource = {
 	.mNext = NULL,
 };
 
+/*
 static otError provisioning_response_send(otMessage *request_message, const otMessageInfo *message_info)
 {
 	otError error = OT_ERROR_NO_BUFS;
@@ -105,6 +108,7 @@ static void provisioning_request_handler(void *context, otMessage *message, cons
 		}
 	}
 }
+*/
 
 static void light_request_handler(void *context, otMessage *message, const otMessageInfo *message_info)
 {
@@ -144,6 +148,7 @@ static void coap_default_handler(void *context, otMessage *message, const otMess
 	LOG_INF("Received CoAP message that does not match any request or resource");
 }
 
+/*
 void ot_coap_activate_provisioning(void)
 {
 	srv_context.provisioning_enabled = true;
@@ -158,13 +163,14 @@ bool ot_coap_is_provisioning_active(void)
 {
 	return srv_context.provisioning_enabled;
 }
+*/
 
 int ot_coap_init(provisioning_request_callback_t on_provisioning_request, light_request_callback_t on_light_request)
 {
 	otError error;
 
-	srv_context.provisioning_enabled = false;
-	srv_context.on_provisioning_request = on_provisioning_request;
+	//srv_context.provisioning_enabled = false;
+	//srv_context.on_provisioning_request = on_provisioning_request;
 	srv_context.on_light_request = on_light_request;
 
 	srv_context.ot = openthread_get_default_instance();
@@ -174,24 +180,22 @@ int ot_coap_init(provisioning_request_callback_t on_provisioning_request, light_
 		goto end;
 	}
 
-	provisioning_resource.mContext = srv_context.ot;
-	provisioning_resource.mHandler = provisioning_request_handler;
+	//provisioning_resource.mContext = srv_context.ot;
+	//provisioning_resource.mHandler = provisioning_request_handler;
 
 	light_resource.mContext = srv_context.ot;
 	light_resource.mHandler = light_request_handler;
 
 	otCoapSetDefaultHandler(srv_context.ot, coap_default_handler, NULL);
 	otCoapAddResource(srv_context.ot, &light_resource);
-	otCoapAddResource(srv_context.ot, &provisioning_resource);
+	//otCoapAddResource(srv_context.ot, &provisioning_resource);
 
 	error = otCoapStart(srv_context.ot, COAP_PORT);
 	if (error != OT_ERROR_NONE) {
 		LOG_ERR("Failed to start OT CoAP. Error: %d", error);
 		goto end;
 	}
-
-    LOG_WRN("all good");
-
+	
 end:
 	return error == OT_ERROR_NONE ? 0 : 1;
 }
