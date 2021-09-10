6b5b32132c3da633c20c265fabdb9295c5aef14f
DankersW <wouter.dankers@skynet.be>
2021-09-10 15:17:01 +0200

    sample: skeleton for printing to USB

---
+++ samples/chip_temp/src/main.c
@@ -0,0 +1,19 @@
+#include <zephyr.h>
+#include <sys/printk.h>
+#include <usb/usb_device.h>
+#include <logging/log.h>
+
+#define LOG_MODULE_NAME usb_print_example
+LOG_MODULE_REGISTER(LOG_MODULE_NAME, LOG_LEVEL_DBG);
+
+void main(void)
+{
+	if (usb_enable(NULL)) {
+		return;
+	}
+	
+	while (1) {
+		k_sleep(K_SECONDS(2));
+		printk("Hello world\n");
+	}
+}
