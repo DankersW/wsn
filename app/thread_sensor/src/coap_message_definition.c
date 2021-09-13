7d5ce53496fae15309bdfe27e597d25d790e0cfc
DankersW <wouter.dankers@skynet.be>
2021-09-13 16:31:44 +0200

    coap: message definition structures

--- app/thread_sensor/src/coap_message_definition.c
+++ app/thread_sensor/src/coap_message_definition.c
@@ -1,1 +1,8 @@
 #include "coap_message_definition.h"
+
+void gen_chip_temp_msg(uint8_t *msg_buf, struct sensor_value tempature_data)
+{
+    msg_buf[0] = THREAD_COAP_TEMP_MSG_CMD;
+    msg_buf[1] = tempature_data.val1;
+    msg_buf[2] = tempature_data.val2 * 0.0001;
+}
--- app/thread_sensor/src/coap_message_definition.h
+++ app/thread_sensor/src/coap_message_definition.h
@@ -1,6 +1,10 @@
 #ifndef COAP_MESSAGE_DEFINITION_H
 #define COAP_MESSAGE_DEFINITION_H
 
+#include <drivers/sensor.h>
 
+#include "ot_definitions.h"
+
+void gen_chip_temp_msg(uint8_t *msg_buf, struct sensor_value tempature_data);
 
 #endif // COAP_MESSAGE_DEFINITION_H
