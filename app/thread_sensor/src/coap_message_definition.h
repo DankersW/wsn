e09eaf71eb7055b91182eb6af8534cb75c013c40
DankersW <wouter.dankers@skynet.be>
2021-09-13 16:55:04 +0200

    coap: message difinition

--- app/thread_sensor/src/coap_message_definition.h
+++ app/thread_sensor/src/coap_message_definition.h
@@ -5,6 +5,8 @@
 
 #include "ot_definitions.h"
 
-void gen_chip_temp_msg(uint8_t *msg_buf, struct sensor_value tempature_data);
+#define CHIP_TEMP_MSG_SIZE 3
+
+void gen_chip_temp_msg(uint8_t *msg_buf, struct sensor_value *tempature_data);
 
 #endif // COAP_MESSAGE_DEFINITION_H
--- app/thread_sensor/src/ot_coap.c
+++ app/thread_sensor/src/ot_coap.c
@@ -47,8 +47,9 @@ static void on_config_request(uint8_t command)
 		dk_set_led_on(TEMP_PUB_LED);
 		send_command = 10;
 		struct sensor_value die_temp = get_chip_temp();
-		uint8_t t_1 = die_temp.val1;
-		coap_send(temp_uri, multicast_local_addr, &t_1, sizeof(t_1));
+		uint8_t msg_buffer[CHIP_TEMP_MSG_SIZE] = {0};
+		gen_chip_temp_msg(msg_buffer, &die_temp);
+		coap_send(temp_uri, multicast_local_addr, msg_buffer, sizeof(msg_buffer));
 		break;
 
 	case THREAD_COAP_TEMP_PUBLISH_OFF_CMD:
