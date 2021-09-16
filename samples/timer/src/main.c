ef719f6d777d0a0092f08985531ff47e8ece68ef
DankersW <wouter.dankers@skynet.be>
2021-09-16 17:43:44 +0200

    samples: button click

--- samples/timer/src/main.c
+++ samples/timer/src/main.c
@@ -2,29 +2,29 @@
 #include <sys/printk.h>
 #include <usb/usb_device.h>
 #include <logging/log.h>
+#include <dk_buttons_and_leds.h>
 
 #define LOG_MODULE_NAME usb_print_example
 LOG_MODULE_REGISTER(LOG_MODULE_NAME, LOG_LEVEL_DBG);
 
+static void on_button_changed(uint32_t button_state, uint32_t has_changed)
+{
+	uint32_t buttons = button_state & has_changed;
+
+	if (buttons & DK_BTN1_MSK) {
+		printk("hello\n");
+	}
+}
+
 void main(void)
 {
-	if (usb_enable(NULL)) {
+	int ret = dk_buttons_init(on_button_changed);
+	if (ret) {
+		LOG_ERR("Cannot init buttons (error: %d)", ret);
 		return;
 	}
-	k_sleep(K_SECONDS(1));
 
-	LOG_ERR("Error message example.");
-	LOG_WRN("Warning message example.");
-	LOG_INF("Info message example.");
-	LOG_DBG("Debug message example.");
-
-	LOG_DBG("Debug message example, %d", 1);
-	LOG_DBG("Debug message example, %d, %d", 1, 2);
-	LOG_DBG("Debug message example, %d, %d, %d", 1, 2, 3);
-	LOG_DBG("Debug message example, %d, %d, %d, 0x%x", 1, 2, 3, 4);
-	
-	while (1) {
-		k_sleep(K_SECONDS(2));
-		printk("Hello world\n");
+	if (usb_enable(NULL)) {
+		return;
 	}
 }
