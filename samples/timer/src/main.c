5d348f76b7d927c241496c704a163e85a6d9201d
DankersW <wouter.dankers@skynet.be>
2021-09-16 18:12:31 +0200

    samples: starting and stopping of timer

--- samples/timer/src/main.c
+++ samples/timer/src/main.c
@@ -7,12 +7,36 @@
 #define LOG_MODULE_NAME usb_print_example
 LOG_MODULE_REGISTER(LOG_MODULE_NAME, LOG_LEVEL_DBG);
 
+bool timer_active = false;
+
+struct k_timer my_timer;
+
+void my_expiry_function(struct k_timer *timer_id)
+{
+	LOG_INF("timer called");
+}
+
+void toggle_timer()
+{
+	timer_active = !timer_active;
+	if (timer_active)
+	{
+		LOG_INF("stating timer");
+		k_timer_start(&my_timer, K_SECONDS(0), K_SECONDS(5));
+	}
+	else
+	{
+		LOG_INF("Stopping timer");
+		k_timer_stop(&my_timer);
+	}
+}
+
 static void on_button_changed(uint32_t button_state, uint32_t has_changed)
 {
 	uint32_t buttons = button_state & has_changed;
 
 	if (buttons & DK_BTN1_MSK) {
-		printk("hello\n");
+		toggle_timer();	
 	}
 }
 
@@ -23,6 +47,8 @@ void main(void)
 		LOG_ERR("Cannot init buttons (error: %d)", ret);
 		return;
 	}
+
+	k_timer_init(&my_timer, my_expiry_function, NULL);
 
 	if (usb_enable(NULL)) {
 		return;
