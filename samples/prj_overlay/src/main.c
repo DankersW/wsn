865334a9a6e443854cc252b4334c445e6d9e819a
DankersW <wouter.dankers@skynet.be>
2021-09-20 13:06:38 +0200

    samples: adding USB overlay support

--- samples/prj_overlay/src/main.c
+++ samples/prj_overlay/src/main.c
@@ -13,16 +13,6 @@ void main(void)
 	}
 	k_sleep(K_SECONDS(1));
 
-	LOG_ERR("Error message example.");
-	LOG_WRN("Warning message example.");
-	LOG_INF("Info message example.");
-	LOG_DBG("Debug message example.");
-
-	LOG_DBG("Debug message example, %d", 1);
-	LOG_DBG("Debug message example, %d, %d", 1, 2);
-	LOG_DBG("Debug message example, %d, %d, %d", 1, 2, 3);
-	LOG_DBG("Debug message example, %d, %d, %d, 0x%x", 1, 2, 3, 4);
-	
 	while (1) {
 		k_sleep(K_SECONDS(2));
 		printk("Hello world\n");
