a321d4bd95dab31b92a16316ef6211af55dbc94f
DankersW <wouter.dankers@skynet.be>
2021-09-20 12:44:02 +0200

    staging

--- app/thread_sensor/src/coap/ot_coap_utils.h
+++ app/thread_sensor/src/coap/ot_coap_utils.h
@@ -21,4 +21,6 @@ typedef void (*config_request_callback_t)(uint8_t cmd);
 int ot_coap_init(light_request_callback_t on_light_request, config_request_callback_t on_config_request);
 int coap_send(const char *const uri[], struct sockaddr_in6 addr, uint8_t *message, uint8_t size);
 
+int test_trans();
+
 #endif
