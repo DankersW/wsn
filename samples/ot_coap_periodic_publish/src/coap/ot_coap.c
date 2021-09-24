184b77e8f5129a712ff674f928863403c558fe82
DankersW <wouter.dankers@skynet.be>
2021-09-24 15:54:53 +0200

    samples: timer work queue moved to main

--- samples/ot_coap_periodic_publish/src/coap/ot_coap.c
+++ samples/ot_coap_periodic_publish/src/coap/ot_coap.c
@@ -14,31 +14,6 @@ static struct sockaddr_in6 multicast_local_addr = {
 	.sin6_scope_id = 0U
 };
 
-bool state = false;
-
-struct k_timer temperature_publicaion_timer;
-struct k_work temperature_publicaion_worker;
-
-static void publication_work_hanlder(struct k_work *work)
-{
-	state = !state;
-	dk_set_led(LIGHT_LED, state);
-	
-	// Program crashes when trying to transmit the message, 
-	// Need to tryout to use otCoapSendRequest to send a message instead, 
-	// since you pass the OT instance to it, hoping it will make it 
-	//struct sensor_value die_temp = get_chip_temp();
-	uint8_t msg_buffer[3] = {0};
-	
-	//coap_send(temp_uri, multicast_local_addr, msg_buffer, sizeof(msg_buffer));
-}
-
-static void publication_timer_expiry_function(struct k_timer *timer_id)
-{
-	k_work_submit(&temperature_publicaion_worker);	
-}
-
-
 static void on_thread_state_changed(uint32_t flags, void *context)
 {
 	struct openthread_context *ot_context = context;
@@ -64,10 +39,6 @@ void init_ot_coap()
 {
 
 	coap_init(AF_INET6, NULL);
-
-	k_timer_init(&temperature_publicaion_timer, publication_timer_expiry_function, NULL);
-	k_work_init(&temperature_publicaion_worker, publication_work_hanlder);
-
 	
     ot_coap_init();
     openthread_set_state_changed_cb(on_thread_state_changed);
--- samples/ot_coap_periodic_publish/src/main.c
+++ samples/ot_coap_periodic_publish/src/main.c
@@ -9,11 +9,42 @@
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
+bool state = false;
+
+struct k_timer temperature_publicaion_timer;
+struct k_work temperature_publicaion_worker;
+
+static void publication_work_hanlder(struct k_work *work)
+{
+	state = !state;
+	dk_set_led(LIGHT_LED, state);
+	
+	// Program crashes when trying to transmit the message, 
+	// Need to tryout to use otCoapSendRequest to send a message instead, 
+	// since you pass the OT instance to it, hoping it will make it 
+	//struct sensor_value die_temp = get_chip_temp();
+	uint8_t msg_buffer[3] = {0};
+
+	LOG_INF("state %d", state);
+	
+	//coap_send(temp_uri, multicast_local_addr, msg_buffer, sizeof(msg_buffer));
+}
+
+static void publication_timer_expiry_function(struct k_timer *timer_id)
+{
+	k_work_submit(&temperature_publicaion_worker);	
+}
+
 void main(void)
 {
 	dk_leds_init();
 
-	init_ot_coap();	
+	k_timer_init(&temperature_publicaion_timer, publication_timer_expiry_function, NULL);
+	k_work_init(&temperature_publicaion_worker, publication_work_hanlder);
 
-	usb_enable(NULL);	
+	init_ot_coap();
+
+	usb_enable(NULL);
+
+	k_timer_start(&temperature_publicaion_timer, K_SECONDS(0), K_SECONDS(5));	
 }
