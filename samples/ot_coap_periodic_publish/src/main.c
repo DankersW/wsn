24d829f3d025b51d122451c6f62b63b99087cc33
DankersW <wouter.dankers@skynet.be>
2021-10-03 15:04:53 +0200

    samples: set and get items from a message queue

--- samples/ot_coap_periodic_publish/src/main.c
+++ samples/ot_coap_periodic_publish/src/main.c
@@ -20,6 +20,17 @@ static void publication_work_hanlder(struct k_work *work);
 static void publication_timer_expiry_function(struct k_timer *timer_id);
 static void on_button_changed(uint32_t button_state, uint32_t has_changed);
 
+
+
+struct data_item_type {
+    bool state;
+	bool something;
+};
+
+char __aligned(4) my_msgq_buffer[10 * sizeof(struct data_item_type)];
+struct k_msgq my_msgq;
+
+
 void main(void)
 {
 	dk_leds_init();
@@ -33,13 +44,16 @@ void main(void)
 	k_timer_init(&temperature_publicaion_timer, publication_timer_expiry_function, NULL);
 	k_work_init(&temperature_publicaion_worker, publication_work_hanlder);
 
+	k_msgq_init(&my_msgq, my_msgq_buffer, sizeof(struct data_item_type), 10);
+
 	init_ot_coap();
 
 	usb_enable(NULL);
 
 	while (true)
 	{
-		k_sleep(K_SECONDS(2));
+		struct data_item_type data;
+		k_msgq_get(&my_msgq, &data, K_FOREVER);
 		if (get_ot_connection_status())
 		{
 			test_send();	
@@ -70,11 +84,13 @@ static void publication_work_hanlder(struct k_work *work)
 	// since you pass the OT instance to it, hoping it will make it 
 	//struct sensor_value die_temp = get_chip_temp();
 
-	LOG_INF("state %d, connected%d", state, get_ot_connection_status());
+	struct data_item_type data = {
+		.state = true,
+		.something = false,
+	};
 
-	//test_send();
-	
-	//coap_send(temp_uri, multicast_local_addr, msg_buffer, sizeof(msg_buffer));
+	int status = k_msgq_put(&my_msgq, &data, K_NO_WAIT);
+	LOG_INF("state %d, connected %d, status %d", state, get_ot_connection_status(), status);
 }
 
 static void publication_timer_expiry_function(struct k_timer *timer_id)
