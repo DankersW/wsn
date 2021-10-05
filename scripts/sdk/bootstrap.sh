e4e622dc8338e22dcaa42b49b1001283059760cd
DankersW <wouter.dankers@skynet.be>
2021-10-05 16:48:34 +0200

    toolchain: moving zephyr docker image to own repo

--- scripts/sdk/bootstrap.sh
+++ scripts/sdk/bootstrap.sh
@@ -4,9 +4,6 @@ BUILDER_TAG="0.1"
 DOCKER_IMAGE="wsn_toolchain"
 TOOLCHAIN_TAG=`./scripts/toolchain/version_toolchain.sh`
 
-echo "INIT SDK"
-./scripts/sdk/run_bootstrap_docker.sh ./scripts/sdk/sdk_init.sh || exit 1
-
 echo "Copy requirements files"
 rm -fr _requirements || exit 1
 mkdir -p _requirements/zephyr _requirements/nrf _requirements/bootloader || exit 1
@@ -19,6 +16,9 @@ docker build --tag dankersw/$DOCKER_IMAGE:$TOOLCHAIN_TAG -f docker/toolchain.doc
 
 echo "Install 3th party tools"
 pip3 install -r requirements.txt
+
+echo "INIT SDK"
+./scripts/toolchain/run.sh west update || exit 1
 
 echo "Cleaning up"
 rm -fr _requirements
--- scripts/sdk/sdk_init.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-west update || exit 1
