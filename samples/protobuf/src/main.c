f0dd895b6e1664ee81e4db7f39d32b9aff97e00c
DankersW <wouter.dankers@skynet.be>
2021-12-22 17:09:50 +0100

    samples: cleanup

--- samples/protobuf/src/main.c
+++ samples/protobuf/src/main.c
@@ -22,8 +22,6 @@ void main(void)
 	while (1) {
 		k_sleep(K_SECONDS(2));
 
-		//proto_encode_decode();
-        
         some_handler();
 	}
 }
@@ -32,7 +30,8 @@ void some_handler()
 {
     uint8_t buffer[128];
     int msg_size = proto_encode(buffer);
-    printk("Message size: %d \n", msg_size);
+    printk("Message encoded successfully with a size: %d \n", msg_size);
+    decode_to_console(buffer, msg_size);
 }
 
 int proto_encode(uint8_t *msg)
@@ -52,56 +51,16 @@ int proto_encode(uint8_t *msg)
     return stream.bytes_written;
 }
 
-
-int proto_encode_decode()
+void decode_to_console(uint8_t *msg, uint8_t size) 
 {
-	/* This is the buffer where we will store our message. */
-    uint8_t buffer[128];
-    size_t message_length;
-    bool status;
-    
-    /* Encode our message */
+    wsn_SensorData message = wsn_SensorData_init_zero;
+    pb_istream_t stream = pb_istream_from_buffer(msg, size);
+    if (!pb_decode(&stream, wsn_SensorData_fields, &message))
     {
-        wsn_SensorData message = wsn_SensorData_init_zero;
-        pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
-        
-		message.temperature = 25.0;
-		message.humidity = 17.77;
-        strcpy(message.sensor_id,"S01");
-        
-        status = pb_encode(&stream, wsn_SensorData_fields, &message);
-        message_length = stream.bytes_written;
-        if (!status)
-        {
-            printf("Encoding failed: %s\n", PB_GET_ERROR(&stream));
-            return 1;
-        }
+        printf("Decoding failed: %s\n", PB_GET_ERROR(&stream));
+        return;
     }
-
-    /* 
-	Now we could transmit the message over network, store it in a file or
-    wrap it to a pigeon's leg.
-    But because we are lazy, we will just decode it immediately. 
-	*/
-    
-    {
-        wsn_SensorData message = wsn_SensorData_init_zero;
-        pb_istream_t stream = pb_istream_from_buffer(buffer, message_length);
-        
-        /* Now we are ready to decode the message. */
-        status = pb_decode(&stream, wsn_SensorData_fields, &message);
-        if (!status)
-        {
-            printf("Decoding failed: %s\n", PB_GET_ERROR(&stream));
-            return 1;
-        }
-        
-        /* Print the data contained in the message. */
-        printf("Sensor ID: %s\n", message.sensor_id);
-        printf("Message lenght: %d\n", message_length);
-        printf("Temperature: %d\n", (int)message.temperature);
-        printf("Humidity: %d\n\n", (int)message.humidity);
-    }
-    
-    return 0;
+    printf("Sensor ID: %s\n", message.sensor_id);
+    printf("Temperature: %d\n", (int)message.temperature);
+    printf("Humidity: %d\n\n", (int)message.humidity);
 }
