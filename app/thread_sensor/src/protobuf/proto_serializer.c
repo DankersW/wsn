a04268ad603336c5289bd4f398ce498b68e3adba
DankersW <wouter.dankers@skynet.be>
2022-02-09 10:52:24 +0100

    ipc: using int instead of float for temp and humi

--- app/thread_sensor/src/protobuf/proto_serializer.c
+++ app/thread_sensor/src/protobuf/proto_serializer.c
@@ -4,9 +4,11 @@ uint8_t serialize_sensor_data(uint8_t *msg_buf, struct sensor_value *tempature_d
 {
     wsn_SensorData message = wsn_SensorData_init_zero;
     pb_ostream_t stream = pb_ostream_from_buffer(msg_buf, PROTO_MSG_MAX_SIZE);
+
+    float temperature = tempature_data->val1 + tempature_data->val2 * 0.000001;
         
-	message.temperature = tempature_data->val1 + tempature_data->val2 * 0.000001;
-	message.humidity = 0.0;
+	message.temperature = (int)(temperature*10);
+	message.humidity = 0;
     strcpy(message.sensor_id, SENSOR_ID);
 
     if (!pb_encode(&stream, wsn_SensorData_fields, &message))
